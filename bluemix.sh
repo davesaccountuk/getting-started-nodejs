8af0078d1987505abb2887c209055ff9447d2134
davecapgemini <dave@here.com>
2016-12-15 10:18:22 +0000

    test

--- bluemix.sh
+++ bluemix.sh
@@ -1,5 +1,5 @@
 echo "Logging in to bluemix CF";
-cf login -u \"$CF_USER\" -p \"$CF_PASS\" -o \"$CF_ORG\" -s \"$CF_SPACE\" -a \"$CF_API\"
+cf login -u $CF_USER -p $CF_PASS -o $CF_ORG -s $CF_SPACE -a $CF_API
 
 echo "Init Bluemix";
 cf ic init
